fee051d1684d075fdab469eca09f176a87b2f227
Tania Molina <mariametropolis8.5@gmail.com>
2017-04-09 12:04:19 -0300

    Add files via upload

---
+++ L_Astra_Lab.ino
@@ -0,0 +1,27 @@
+//OSCILLOSCOPE LAB + ArduinoUno
+
+#define Potentiometer A2
+#define LED 13
+   
+int val = 10;        
+
+void setup() {
+  pinMode(LED, OUTPUT);  
+}
+
+void loop() {
+  val = analogRead(Potentiometer);    
+  digitalWrite(LED, HIGH);  
+  delay(val);                  
+  digitalWrite(LED, LOW);  
+  delay(val);                  
+}
+
+/*OP AMP:
+
+To use the oscilloscope with other components you have to wire the positive input
+of the oscilloscope to the Vout of the Operational Amplifier and the negative
+input of the oscilloscope to the positive input of the Op Amp (to get an Inverter Circuit).
+
+*/
+
