fada25e2029cba6198c101a79085817a5fbc1651
Niam Moltta <lastralab@gmail.com>
2017-09-29 12:30:04 -0300

    Update BodaShirt.ino

--- BodaShirt.ino
+++ BodaShirt.ino
@@ -6,7 +6,7 @@
 #define AthleticBodyType 1 //LOL
 #define TeamWork 10
 
-int Tania = (Patient + Adventurous + Trustworthy + Stubborn + AthleticBodyType);
+int Niam = (Patient + Adventurous + Trustworthy + Stubborn + AthleticBodyType);
 int Grace = (Patient + Adventurous + Trustworthy + Stubborn + AthleticBodyType);
 
 void setup() {
